581143315f8f1850628dbc6af2d1b4d41f379d0d
Albertluiiz <luisguillen290301@gmail.com>
2023-06-25 00:47:09 -0500

    image

---
+++ dockerfile-Crear imagenes/teoria.php
@@ -0,0 +1,20 @@
+<!-- Dockerfile -->
+Un archivo de texto con instrucciones necesarias para crear una imagen. se puede ver como un blueprint o plano para su construcción.
+
+<!-- Temas puntuales de la sección -->
+Esta sección es sumamente importante para la comprensión y creación de imágenes personalizadas.
+
+Aquí veremos:
+Dockerfile
+.dockerignore
+<!-- Principales comandos del Dockerfile -->
+FROM
+RUN
+COPY
+WORKDIR
+Entre otros
+Asignar tags
+Build
+Buildx (Para múltiples arquitecturas)
+
+Es una sección indispensable para poder crear de forma eficiente nuestras futuras imágenes que desplegaremos en servidores con arquitecturas diferentes a la nuestra computadora.
